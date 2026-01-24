6ac56df03d8f29d4de04c684568c8e7e5dd74161
KindMinotaur <shaun.belknap@gmail.com>
2026-01-24 14:12:26 -0600

    Finished initial run_python function

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from prompts import system_prompt
+
 
 def main():
     parser = argparse.ArgumentParser(description="AI Code Assistant")
@@ -29,6 +31,10 @@ def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt,
+            temperature=0,
+        ),
     )
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
